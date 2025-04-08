239ff10185ced4155fc5a19c4df387f447681f85
julio <158296107+jul213@users.noreply.github.com>
2025-04-08 09:33:44 +0000

    exportamos un modulo que es una funcion Buenas

--- servidor.js
+++ servidor.js
@@ -4,3 +4,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {"content-type": "text/html"});
   res.end("hola mundo");
 }).listen(8080)
+
+
+exports.Buenas = function(){
+  return Date();
+}
