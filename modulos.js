473296e25494548228cf672862159b96102cba70
julio <158296107+jul213@users.noreply.github.com>
2025-04-08 09:38:14 +0000

    usamos el modulo en el servidor

--- modulos.js
+++ modulos.js
@@ -6,6 +6,6 @@ var http = require("http");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {"content-type": "text/html"});
-  
+  res.write("la fecha del servidor es: " + dt.Buenas())
   res.end("hola mundo");
 }).listen(8080)
