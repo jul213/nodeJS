adf0f32b001ac559faa447618160d4a43f697685
julio <158296107+jul213@users.noreply.github.com>
2025-04-08 10:16:59 +0000

    creacion de transporter

--- correo.js
+++ correo.js
@@ -1,3 +1,10 @@
 var nodemailer = require("nodemailer");
 
 
+var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "juan@gmail.com",
+        pass: "suizo520"
+    }
+});
