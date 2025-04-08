483825a102a3e3cf92fa7ac32a601693b004f3cc
julio <158296107+jul213@users.noreply.github.com>
2025-04-08 10:02:19 +0000

    encabezado

--- fileServer.js
+++ fileServer.js
@@ -6,5 +6,11 @@ var fs = require("fs");
 http.createServer(function(req,res){
     var q = url.parse(req.url,true);
     var filename = "." + q.pathname;
-    
+    fs.readFile(filename, function(err,data){
+        if (err){
+            res.writeHead(404, {"content-type": "text/html"})
+        }
+
+
+    })
 })
