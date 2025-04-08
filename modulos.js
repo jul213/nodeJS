bc8a607342611a788dd0c7b10117469e9738064f
julio <158296107+jul213@users.noreply.github.com>
2025-04-08 09:37:32 +0000

    importamos nuestro modulo

--- modulos.js
+++ modulos.js
@@ -1,6 +1,11 @@
+var dt = require("./servidor")
+
+
+
 var http = require("http");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {"content-type": "text/html"});
+  
   res.end("hola mundo");
 }).listen(8080)
